421f80895d8c3df3babc6988f11fbb5631016756
khumaryn <119412655+khumaryn@users.noreply.github.com>
2023-01-12 01:47:40 +0400

    Add files via upload

---
+++ refl/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REFL
+{
+    internal class Product
+    {
+        public Product(double costPrice)
+        {
+            _costPrice = costPrice;
+        }
+        private double _costPrice;
+        public double salePrice;
+
+        public string Name;
+        public string Category { get; set; }
+
+
+    }
+}
---
+++ refl/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace REFL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(4)
+            {
+                Name = "fhj",
+                salePrice = 454,
+
+
+            };
+            var type = product.GetType();
+            foreach (var item in type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                if (item.Name == "_costPrice")
+                {
+                    item.SetValue(product, 100);
+                }
+                Console.WriteLine($"{item.Name} - {item.GetValue(product)}");
+            }
+            foreach (var item in type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                Console.WriteLine($"{item.Name} - {item.GetValue(product)}");
+            }
+     
+            foreach (var item in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                Console.WriteLine(item.Name);
+            }
+            var asemmblies = Assembly.GetExecutingAssembly();
+
+
+            Console.WriteLine("All types");
+            foreach (var item in asemmblies.GetTypes())
+            {
+                Console.WriteLine(item.Name + " - " + item.Namespace);
+                foreach (var field in item.GetFields())
+                {
+                    Console.WriteLine("\t" + field.Name);
+                }
+            }
+        }
+    }
+}
